01044cdf56983cd5fb61f09481685788189a4b2f
Alexander Sabanov <alexsab@mail.ru>
2024-05-20 21:49:06 +0400

    upd astro config

--- astro.local.config.mjs
+++ astro.local.config.mjs
@@ -10,7 +10,7 @@ import icon from "astro-icon";
 export default defineConfig({
 	integrations: [
 		tailwind({
-			configFile: './tailwind.infiniti.config.js'
+			configFile: './tailwind.infiniti.js'
 		}),
 		sitemap(),
 		robots(),
